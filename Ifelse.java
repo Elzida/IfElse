b6a9c3767ab1676775cd54776a36f2a6380800d8
Elzida <152496247+Elzida@users.noreply.github.com>
2023-12-06 23:31:37 +0600

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,15 @@
+public class Ifelse {
+
+        public static void main(String[] args) {
+
+            int a = 15; int b = 23;
+            if (a==b) {
+            System.out.println(a+b);
+            }
+            else {
+                System.out.println(a-b);
+            }
+        }
+
+
+}
